179ed5fa50114278217015482388caaaca0243b9
Mathieu Breton <mathieu.breton@js-republic.com>
2017-11-15 17:06:48 +0100

    add material, home page and fix tests

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,29 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
-  </div>
+<div id="app">
+  <md-whiteframe md-elevation="3" class="main-toolbar">
+    <md-toolbar>
+      <div class="md-toolbar-container">
+        <md-button class="md-icon-button" @click="$refs.sidebar.toggle()">
+          <md-icon>menu</md-icon>
+        </md-button>
+        <h2 class="md-title">JS-Talks</h2>
+  
+        <span style="flex: 1"></span>
+  
+        <span>> Mathieu</span>
+        <md-button class="md-icon-button">
+          <md-icon>view_module</md-icon>
+        </md-button>
+      </div>
+    </md-toolbar>
+  </md-whiteframe>
+  
+  <main class="main-content">
+    <md-sidenav class="md-left md-fixed" ref="sidebar">  
+    </md-sidenav>
+    <router-view></router-view>
+  </main>
+</div>
 </template>
 
 <script>
@@ -11,13 +32,19 @@ export default {
 }
 </script>
 
-<style>
+<style lang="scss">
+html,
+body,
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  height: 100%;
+  overflow: hidden;
+}
+
+#app {
+  display: flex;
+  flex-flow: column;
+  .main-content{
+    padding: 2rem;
+  }
 }
 </style>
